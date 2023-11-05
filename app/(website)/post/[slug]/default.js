ed47a1f8aea6fa1cff1ffe3109dc5ee1a6c59744
Athman77 <67540110+Athman77@users.noreply.github.com>
2023-11-05 17:19:41 +0300

    Update default.js

--- app/(website)/post/[slug]/default.js
+++ app/(website)/post/[slug]/default.js
@@ -5,7 +5,10 @@ import { notFound } from "next/navigation";
 import { PortableText } from "@/lib/sanity/plugins/portabletext";
 import { urlForImage } from "@/lib/sanity/image";
 import { parseISO, format } from "date-fns";
-
+import Footer from "@/components/footer";
+import Header from "@/components/header";
+import PostList from "@/components/postlist";
+import Subscribe from "@/components/subscribe";
 import CategoryLabel from "@/components/blog/category";
 import AuthorCard from "@/components/blog/authorCard";
 
@@ -28,82 +31,120 @@ export default function Post(props) {
 
   return (
     <>
-      <Container className="!pt-0">
-        <div className="mx-auto max-w-screen-md ">
-          <div className="flex justify-center">
-            <CategoryLabel categories={post.categories} />
-          </div>
+               <div className="global-cover"></div>
 
-          <h1 className="text-brand-primary mb-3 mt-2 text-center text-3xl font-semibold tracking-tight dark:text-white lg:text-4xl lg:leading-snug">
-            {post.title}
-          </h1>
+      <div className="global-wrap">
+        <div className="global-content">
+          <Header />
 
-          <div className="mt-3 flex justify-center space-x-3 text-gray-500 ">
-            <div className="flex items-center gap-3">
-              <div className="relative h-10 w-10 flex-shrink-0">
-                {AuthorimageProps && (
-                  <Link href={`/author/${post.author.slug.current}`}>
-                    <Image
-                      src={AuthorimageProps.src}
-                      alt={post?.author?.name}
-                      className="rounded-full object-cover"
-                      fill
-                      sizes="40px"
-                    />
-                  </Link>
-                )}
-              </div>
-              <div>
-                <p className="text-gray-800 dark:text-gray-400">
-                  <Link href={`/author/${post.author.slug.current}`}>
-                    {post.author.name}
-                  </Link>
-                </p>
-                <div className="flex items-center space-x-2 text-sm">
-                  <time
-                    className="text-gray-500 dark:text-gray-400"
-                    dateTime={post?.publishedAt || post._createdAt}>
-                    {format(
-                      parseISO(post?.publishedAt || post._createdAt),
-                      "MMMM dd, yyyy"
-                    )}
-                  </time>
-                  <span>· {post.estReadingTime || "5"} min read</span>
+          <main className="global-main">
+            <progress className="post-progress"></progress>
+
+            <article className="post-section">
+              <div className="post-header post-full-imageB">
+                <div className="post-header-content">
+                  <div className="post-tags global-tags">
+                    <a href="">Lifestyle</a>
+                    <a href="">Forex Trading</a>
+                  </div>
+                  <h1
+                    id="post-title"
+                    className="post-title global-title-big global-title-offset"
+                  >
+                    {" "}
+                    {post.title}
+                  </h1>
+                  <div className="post-authors global-authors">
+                    <div className="post-author-item global-author-item global-image is-image">
+                      <a
+                        href="/author/victoria/"
+                        className="global-link"
+                        aria-label="Victoria West"
+                      ></a>
+                      {AuthorimageProps && (
+                        <img
+                          src={AuthorimageProps.src}
+                          loading="lazy"
+                          alt="Victoria West"
+                        />
+                      )}
+                    </div>
+                    <div className="post-author-item global-author-item global-image is-image is-last">
+                      <a
+                        href="/author/hannah/"
+                        className="global-link"
+                        aria-label="Hannah Weastell"
+                      ></a>
+                      <img
+                        src="https://cdn.sanity.io/images/cwzkjmps/production/f271a0e60dc0ec7a41a2992e5ce28a4bb65c0ced-400x400.jpg"
+                        loading="lazy"
+                        alt="Hannah Weastell"
+                      />
+                    </div>
+                    <div className="post-meta global-meta">
+                      <a href="/author/victoria/"></a>
+                      <a href="/author/hannah/"> {post.author.name}</a>
+                      <time>
+                        {format(
+                          parseISO(post?.publishedAt || post._createdAt),
+                          "MMMM dd, yyyy"
+                        )}{" "}
+                        · {post.estReadingTime || "5"} minutes read
+                      </time>
+                    </div>
+                  </div>
                 </div>
+                <figure className="post-image">
+                  {imageProps && (
+                    <img
+                      sizes="(max-width:480px) 400px, (max-width:1440px) 800px, 1600px"
+                      src={imageProps.src}
+                      alt="Look at life with the eyes of a child"
+                    />
+                  )}
+                </figure>
+              </div>
+
+              <div className="post-content">
+                {post.body && <PortableText value={post.body} />}
+              </div>
+            </article>
+
+            <div className="special-section">
+              <div className="global-title-wrap">
+                <h3 className="global-title-medium">
+                  Explore More<span>Blogs</span>
+                </h3>
+                <Link href="/archive">
+                  <a>
+                    <svg
+                      role="img"
+                      viewBox="0 0 24 24"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path d="M17.185 6.08c1.485 1.585 2.733 2.8 3.813 3.643 1.045.843 2.057 1.45 3.002 1.855v.743c-1.08.506-2.16 1.18-3.171 2.024-1.012.81-2.227 1.99-3.644 3.576h-1.282c1.046-2.193 2.26-4.217 3.374-5.432H.014v-.978h19.263a31.274 31.274 0 01-1.855-2.699c-.338-.506-.844-1.417-1.485-2.733h1.248z"></path>
+                    </svg>
+                  </a>
+                </Link>
+              </div>
+
+              <div class="special-wrap">
+                {/* {templateBlog.slice(0, 4).map((post) => (
+                      <PostList
+                        key={post._id}
+                        post={post}
+                        aspect="landscape"
+                        preloadImage={true}
+                      />
+                    ))}*/}
               </div>
             </div>
-          </div>
+
+            <Subscribe />
+          </main>
+          <Footer />
         </div>
-      </Container>
-
-      <div className="relative z-0 mx-auto aspect-video max-w-screen-lg overflow-hidden lg:rounded-lg">
-        {imageProps && (
-          <Image
-            src={imageProps.src}
-            alt={post.mainImage?.alt || "Thumbnail"}
-            loading="eager"
-            fill
-            sizes="100vw"
-            className="object-cover"
-          />
-        )}
       </div>
-
-      <Container>
-        <article className="mx-auto max-w-screen-md ">
-          <div className="prose mx-auto my-3 dark:prose-invert prose-a:text-blue-600">
-            {post.body && <PortableText value={post.body} />}
-          </div>
-          <div className="mb-7 mt-7 flex justify-center">
-            <Link
-              href="/"
-              className="bg-brand-secondary/20 rounded-full px-5 py-2 text-sm text-blue-600 dark:text-blue-500 ">
-              ← View all posts
-            </Link>
-          </div>
-          {post.author && <AuthorCard author={post.author} />}
-        </article>
-      </Container>
     </>
   );
 }
