bbdbe6ef88288744a8d16e68300b7e498021d13e
Athman77 <67540110+Athman77@users.noreply.github.com>
2023-11-06 21:02:28 +0300

    Update layout.tsx

--- app/(website)/layout.tsx
+++ app/(website)/layout.tsx
@@ -9,36 +9,30 @@ export async function sharedMetaData(params) {
   return {
     // metadataBase: new URL(settings.url),
     title: {
-      default:
-        settings?.title ||
-        "Stablo - Blog Template for Next.js & Sanity CMS",
-      template: "%s | Stablo"
+      default: settings?.title || "BigTech - Forex Trading Blog & Trading Analysis",
+      template: "%s | BigTech",
     },
-    description:
-      settings?.description ||
-      "Stablo - popular open-source next.js and sanity blog template",
-    keywords: ["Next.js", "Sanity", "Tailwind CSS"],
-    authors: [{ name: "Surjith" }],
-    canonical: settings?.url,
+    description: "Stay updated with the latest news and analysis in Forex trading. Learn strategies, get insights, and make informed decisions",
+    keywords: ["Trading Blog", "Trading Strategies", "Trading Analysis"],
+    authors: [{ name: "Gaucho" }],
+    canonical: settings.url,
     openGraph: {
       images: [
         {
-          url:
-            urlForImage(settings?.openGraphImage)?.src ||
-            "/img/opengraph.jpg",
+          url: "https://cdn.sanity.io/images/cwzkjmps/production/a03cf28eead83f5e47e9a3eb309616d7e1b6fb97-1014x760.png",
           width: 800,
-          height: 600
-        }
-      ]
+          height: 600,
+        },
+      ],
     },
     twitter: {
-      title: settings?.title || "Stablo Template",
-      card: "summary_large_image"
+      title: settings?.title,
+      card: "summary_large_image",
     },
     robots: {
       index: true,
-      follow: true
-    }
+      follow: true,
+    },
   };
 }
 
