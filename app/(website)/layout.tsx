064f155f3148699b322ac88f2d8f5a647e011123
Athman77 <67540110+Athman77@users.noreply.github.com>
2023-11-08 21:49:56 +0300

    Update layout.tsx

--- app/(website)/layout.tsx
+++ app/(website)/layout.tsx
@@ -1,38 +1,46 @@
+
 import { getSettings } from "@/lib/sanity/client";
 import Footer from "@/components/footer";
 import { urlForImage } from "@/lib/sanity/image";
 import Navbar from "@/components/navbar";
 
-export async function sharedMetaData(params) {
+async function sharedMetaData(params) {
   const settings = await getSettings();
 
   return {
+    // enable this for resolving opengraph image
     // metadataBase: new URL(settings.url),
     title: {
-      default: settings?.title || "BigTech - Forex Trading Blog & Trading Analysis",
-      template: "%s | BigTech",
+      default:
+        settings?.title ||
+        "NextApp - Blog Template for Next.js & Sanity CMS",
+      template: "%s | Stablo"
     },
-    description: "Stay updated with the latest news and analysis in Forex trading. Learn strategies, get insights, and make informed decisions",
-    keywords: ["Trading Blog", "Trading Strategies", "Trading Analysis"],
-    authors: [{ name: "Gaucho" }],
-    canonical: settings.url,
+    description:
+      settings?.description ||
+      "Stablo - popular open-source next.js and sanity blog template",
+    keywords: ["Next.js", "Sanity", "Tailwind CSS"],
+    authors: [{ name: "Surjith" }],
+    canonical: settings?.url,
     openGraph: {
       images: [
         {
-          url: "https://cdn.sanity.io/images/cwzkjmps/production/a03cf28eead83f5e47e9a3eb309616d7e1b6fb97-1014x760.png",
+          url:
+            urlForImage(settings?.openGraphImage)?.src ||
+            "/img/opengraph.jpg",
           width: 800,
-          height: 600,
-        },
-      ],
+          height: 600
+        }
+      ]
     },
     twitter: {
-      title: settings?.title,
-      card: "summary_large_image",
+      title: settings?.title || "Stablo Template",
+      card: "summary_large_image"
     },
     robots: {
       index: true,
-      follow: true,
-    },
+      follow: true
+    }
   };
 }
 
@@ -44,13 +52,13 @@ export default async function Layout({ children, params }) {
   const settings = await getSettings();
   return (
     <>
-      
+      {/* <Navbar {...settings} />*/}
 
       <div>{children}</div>
 
-      
+      {/*<Footer {...settings} />*/}
     </>
   );
 }
 // enable revalidate for all pages in this layout
- export const revalidate = 10;
+export const revalidate = 10;
