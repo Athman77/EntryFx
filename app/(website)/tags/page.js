3b6000cf6a6a5974fff80c09a0c9e70c915bd29a
Athman77 <athmangaucho77@gmail.com>
2024-01-13 09:27:12 +0300

    tags

---
+++ app/(website)/tags/page.js
@@ -0,0 +1,24 @@
+import { Suspense } from "react";
+import Container from "@/components/container";
+import Archive from "./archive";
+import Loading from "@/components/loading";
+
+export const dynamic = "force-dynamic";
+
+export const runtime = "edge";
+
+export default async function ArchivePage({ searchParams }) {
+  return (
+    <>
+
+        <Suspense
+          key={searchParams.page || "1"}
+          fallback={<Loading />}>
+          <Archive searchParams={searchParams} />
+        </Suspense>
+    
+    </>
+  );
+}
+
+// export const revalidate = 60;
