148e8ff3a0c69781aa30ad81879ab768d6fd8801
Athman77 <athmangaucho77@gmail.com>
2024-01-13 17:29:11 +0300

    dd

--- app/(website)/tags/archive.js
+++ app/(website)/tags/archive.js
@@ -3,6 +3,11 @@ import Header from "@/components/header";
 
 import Link from "next/link";
 export default function Tags({ posts }) {
+  const quran = posts.filter(post => post.tags === "quran");
+  const reminder = posts.filter(post => post.tags === "reminder");
+  const lifeStyle = posts.filter(post => post.tags === "lifeStyle");
+  const prayer = posts.filter(post => post.tags === "prayer");
+  const stories = posts.filter(post => post.tags === "stories");
   return (
     <>
       <div className="global-cover"></div>
@@ -37,7 +42,7 @@ export default function Tags({ posts }) {
                   <span>{posts?.length} posts</span>
                 </div>
                 <div className="custom-archive-item">
-                  <Link href="/archive">
+                  <Link href="/life">
                     <a className="global-image">
                       <img
                         src="https://cdn.sanity.io/images/11ognen8/production/e3b0790ad874f9f0436391aa924b44ee09183a3c-736x1104.jpg"
@@ -47,53 +52,66 @@ export default function Tags({ posts }) {
                     </a>
                   </Link>
                   <h2>
-                    <Link href="/archive">
+                    <Link href="/life">
                       <a>Lifestyle</a>
                     </Link>
                   </h2>
-                  <span>5 posts</span>
+                  <span>{lifeStyle?.length} posts</span>
                 </div>
                 <div className="custom-archive-item">
-                  <a href="/tag/lifestyle/" className="global-image">
-                    <img
-                      src="https://cdn.sanity.io/images/11ognen8/production/eb084bdb09738b134db97f7d5f3cb5c5edb1ca08-736x1281.jpg"
-                      loading="lazy"
-                      alt="Lifestyle"
-                    />{" "}
-                  </a>
+                  <Link href="/stories">
+                    <a className="global-image">
+                      <img
+                        src="https://cdn.sanity.io/images/11ognen8/production/eb084bdb09738b134db97f7d5f3cb5c5edb1ca08-736x1281.jpg"
+                        loading="lazy"
+                        alt="Lifestyle"
+                      />{" "}
+                    </a>
+                  </Link>
                   <h2>
-                    <a href="/tag/lifestyle/">Islamic Stories</a>
+                    <Link href="/stories">
+                      <a>Islamic Stories</a>
+                    </Link>
                   </h2>
-                  <span>9 posts</span>
+
+                  <span>{stories?.length} posts</span>
                 </div>
                 <h3 className="custom-archive-heading global-title-small">
                   <span>See also</span>
                 </h3>
                 <div className="custom-archive-item bottom">
-                  <a href="/tag/review/" className="global-image">
-                    <img
-                      src="https://cdn.sanity.io/images/11ognen8/production/3e847aaad1dd24c5db3e788f1a20b7261ca87e55-736x736.jpg"
-                      loading="lazy"
-                      alt="Review"
-                    />{" "}
-                  </a>
+                  <Link href="/reminder">
+                    <a className="global-image">
+                      <img
+                        src="https://cdn.sanity.io/images/11ognen8/production/3e847aaad1dd24c5db3e788f1a20b7261ca87e55-736x736.jpg"
+                        loading="lazy"
+                        alt="Review"
+                      />{" "}
+                    </a>
+                  </Link>
                   <h2>
-                    <a href="/tag/review/">Daily Reminder</a>
+                    <Link href="/reminder">
+                      <a>Daily Reminder</a>
+                    </Link>
                   </h2>
-                  <span>6 posts</span>
+                  <span>{reminder?.length} posts</span>
                 </div>
                 <div className="custom-archive-item bottom">
-                  <a href="/tag/people/" className="global-image">
-                    <img
-                      src="https://cdn.sanity.io/images/11ognen8/production/b57313af4eb48dc5cde435966e35fa03bdb8245f-611x1086.jpg"
-                      loading="lazy"
-                      alt="People"
-                    />{" "}
-                  </a>
+                  <Link href="/prayer">
+                    <a className="global-image">
+                      <img
+                        src="https://cdn.sanity.io/images/11ognen8/production/b57313af4eb48dc5cde435966e35fa03bdb8245f-611x1086.jpg"
+                        loading="lazy"
+                        alt="People"
+                      />{" "}
+                    </a>
+                  </Link>
                   <h2>
-                    <a href="/tag/people/">Dua & Prayers</a>
+                    <Link href="/prayer">
+                      <a>Dua & Prayers</a>
+                    </Link>
                   </h2>
-                  <span>5 posts</span>
+                  <span>{prayer?.length} posts</span>
                 </div>
                 <div className="custom-archive-item bottom">
                   <Link href="/quran">
@@ -110,7 +128,7 @@ export default function Tags({ posts }) {
                       <a>Quran Recitation</a>
                     </Link>
                   </h2>
-                  <span>4 posts</span>
+                  <span>{quran?.length} posts</span>
                 </div>
               </div>
             </div>
