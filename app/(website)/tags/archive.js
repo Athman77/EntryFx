641e866f7734154cde4812dd0c17c63f903fd5ad
Athman77 <athmangaucho77@gmail.com>
2024-01-13 09:47:29 +0300

    tags

--- app/(website)/tags/archive.js
+++ app/(website)/tags/archive.js
@@ -44,39 +44,39 @@ export default async function Post({ searchParams }) {
  <div className="custom-archive-wrap loop-wrap">
    <div className="custom-archive-item">
      <a href="/tag/creative/" className="global-image">
-       <img src="images/veronika-jorjobert-UU62Fwb5VxQ-unsplash-1.jpg" loading="lazy" alt="Creative" /> </a>
-     <h2><a href="/tag/creative/">Creative</a></h2>
+       <img src="https://cdn.sanity.io/images/11ognen8/production/b40108ca9bb60da852779daf553cf78e95a67089-736x736.jpg" loading="lazy" alt="Creative" /> </a>
+     <h2><a href="/tag/creative/">All Posts</a></h2>
      <span>10 posts</span>
    </div>
    <div className="custom-archive-item">
      <a href="/tag/art/" className="global-image">
-       <img src="images/allec-gomes-6J2zxXbjcxo-unsplash.jpg" loading="lazy" alt="Art" /> </a>
-     <h2><a href="/tag/art/">Art</a></h2>
+       <img src="https://cdn.sanity.io/images/11ognen8/production/e3b0790ad874f9f0436391aa924b44ee09183a3c-736x1104.jpg" loading="lazy" alt="Art" /> </a>
+     <h2><a href="/tag/art/">Lifestyle</a></h2>
      <span>10 posts</span>
    </div>
    <div className="custom-archive-item">
      <a href="/tag/lifestyle/" className="global-image">
-       <img src="images/laura-adai-eHdRLiazcww-unsplash.jpg" loading="lazy" alt="Lifestyle" /> </a>
-     <h2><a href="/tag/lifestyle/">Lifestyle</a></h2>
+       <img src="https://cdn.sanity.io/images/11ognen8/production/eb084bdb09738b134db97f7d5f3cb5c5edb1ca08-736x1281.jpg" loading="lazy" alt="Lifestyle" /> </a>
+     <h2><a href="/tag/lifestyle/">Islamic Stories</a></h2>
      <span>9 posts</span>
    </div>
    <h3 className="custom-archive-heading global-title-small"><span>See also</span></h3>
    <div className="custom-archive-item bottom">
      <a href="/tag/review/" className="global-image">
-       <img src="images/photo-1612599316791-451087c7fe15.jpeg" loading="lazy" alt="Review" /> </a>
-     <h2><a href="/tag/review/">Review</a></h2>
+       <img src="https://cdn.sanity.io/images/11ognen8/production/3e847aaad1dd24c5db3e788f1a20b7261ca87e55-736x736.jpg" loading="lazy" alt="Review" /> </a>
+     <h2><a href="/tag/review/">Daily Reminder</a></h2>
      <span>6 posts</span>
    </div>
    <div className="custom-archive-item bottom">
      <a href="/tag/people/" className="global-image">
-       <img src="images/photo-1585007600263-71228e40c8d1.jpeg" loading="lazy" alt="People" /> </a>
-     <h2><a href="/tag/people/">People</a></h2>
+       <img src="https://cdn.sanity.io/images/11ognen8/production/b57313af4eb48dc5cde435966e35fa03bdb8245f-611x1086.jpg" loading="lazy" alt="People" /> </a>
+     <h2><a href="/tag/people/">Dua & Prayers</a></h2>
      <span>5 posts</span>
    </div>
    <div className="custom-archive-item bottom">
      <a href="/tag/idea/" className="global-image">
-       <img src="images/photo-1611095564350-2cbe940a8d99.jpeg" loading="lazy" alt="Idea" /> </a>
-     <h2><a href="/tag/idea/">Idea</a></h2>
+       <img src="https://cdn.sanity.io/images/11ognen8/production/4db7c7a7e98225f1b003b93c5bebf6b197493b15-433x639.jpg" loading="lazy" alt="Idea" /> </a>
+     <h2><a href="/tag/idea/">Quran Recitation</a></h2>
      <span>4 posts</span>
    </div>
  </div>
