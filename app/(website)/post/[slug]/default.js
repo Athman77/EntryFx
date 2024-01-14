a3fabd51279dc548db1efdac4340ac29193514c6
Athman77 <athmangaucho77@gmail.com>
2024-01-15 00:46:41 +0300

    EEE

--- app/(website)/post/[slug]/default.js
+++ app/(website)/post/[slug]/default.js
@@ -119,12 +119,18 @@ export default function Post(props) {
                       />
                     </video>
                   ) : (
-                    <ReactPlayer
-                    className="react-player"
-                    controls
-                     // url={`https://www.youtube.com/watch?v=${post?.youtube}`}
-                     
-                    />
+                    <iframe
+                    width="100%"
+                    height="500px"
+                   
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                    allowFullscreen
+                    title="YouTube video player"
+                    frameborder="0"
+                    src={`https://www.youtube.com/embed/${post?.youtube}?enablejsapi=1&amp;origin=http%3A%2F%2Flocalhost%3A3000&amp;widgetid=1`}
+                   
+                    
+                  ></iframe>
                   )}
                 </figure>
               </div>
