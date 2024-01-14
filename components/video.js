96e1e087be044874ea9fbe3c558b73effcb4b51b
Athman77 <athmangaucho77@gmail.com>
2024-01-15 02:00:49 +0300

    EE

--- components/video.js
+++ components/video.js
@@ -1,25 +1,25 @@
-
 "use client";
-import React, { useState } from 'react';
-
+import React, { useState } from "react";
 
 import ReactPlayer from "react-player";
 
-
 //import videoImg from 'assets/video.png';
 
-const VideoOne = ({id}) => {
+const VideoOne = ({ id }) => {
   const [videoOpen, setVideoOpen] = useState(false);
-  const handleClick = (e) => {
+  const handleClick = e => {
     e.preventDefault();
     setVideoOpen(true);
   };
   return (
     <>
-    <ReactPlayer url={`https://www.youtube.com/watch?v=${id}`} className="react-playerk vida" controls />
+      <ReactPlayer
+        url={`https://www.youtube.com/watch?v=${id}`}
+        className="react-playerk vida"
+        controls
+      />
     </>
   );
 };
 
 export default VideoOne;
-
