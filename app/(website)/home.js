bc9d124fd987fb95f9059c4aaf8a5769b78a3143
Athman77 <67540110+Athman77@users.noreply.github.com>
2024-04-25 21:58:25 +0300

    Update home.js

--- app/(website)/home.js
+++ app/(website)/home.js
@@ -1,4 +1,5 @@
 
+
 import Link from "next/link";
 import Container from "@/components/container";
 import PostList from "@/components/postlist";
@@ -197,7 +198,7 @@ export default function Post({ posts }) {
                             ))}
                       </div>
 
-                      {/*  <nav className="bt-pagination" role="navigation">
+                       <nav className="bt-pagination" role="navigation">
                         {" "}
                         <span
                           aria-current="page"
@@ -224,7 +225,7 @@ export default function Post({ posts }) {
                             <path d="M9.28111 0.217951L8.54638 0.925058C8.37249 1.09242 8.37249 1.36377 8.54638 1.53117L14.3032 7.07149L1.63283 7.07149C1.38691 7.07149 1.18752 7.26338 1.18752 7.50006L1.18752 8.50006C1.18752 8.73674 1.38691 8.92863 1.63283 8.92863L14.3032 8.92863L8.54638 14.469C8.37249 14.6363 8.37249 14.9077 8.54638 15.0751L9.28111 15.7822C9.455 15.9496 9.73696 15.9496 9.91089 15.7822L17.6821 8.30309C17.856 8.13574 17.856 7.86438 17.6821 7.69699L9.91086 0.217952C9.73696 0.0505587 9.455 0.0505586 9.28111 0.217951Z"></path>
                           </svg>
                         </a>
-                      </nav>*/}
+                      </nav>
                     </div>
                   </div>
                 </div>
@@ -266,7 +267,22 @@ export default function Post({ posts }) {
 
         {/*--_-gaucho---**/}
       </div>
+  <div className="global-wrap">
+        <div className="global-content">
+          {/*** header ***/}
 
+          <Header />
+          {/*** end ***/}
+
+          <main className="global-main">
+       
+
+            <Subscribe />
+          </main>
+
+          <Footer / >
+        </div>
+      </div>
       {/***** end *****/}
     </>
   );
