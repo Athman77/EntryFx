8a0786cfc036118cc48b5ef803aaeeec7b82a519
Athman77 <67540110+Athman77@users.noreply.github.com>
2024-04-25 22:08:35 +0300

    Update home.js

--- app/(website)/home.js
+++ app/(website)/home.js
@@ -210,9 +210,10 @@ export default function Post({ posts }) {
                           <a className="page-numbers">2</a>{" "}
                         </Link>
                         <a className="page-numbers">3</a>{" "}
+                            <Link href="/archive">
                         <a
                           className="next page-numbers"
-                          href="https://influencer.beplusthemes.com/showreel-podcasts-style-03/page/2/"
+                         
                         >
                           Next
                           <svg
@@ -225,6 +226,7 @@ export default function Post({ posts }) {
                             <path d="M9.28111 0.217951L8.54638 0.925058C8.37249 1.09242 8.37249 1.36377 8.54638 1.53117L14.3032 7.07149L1.63283 7.07149C1.38691 7.07149 1.18752 7.26338 1.18752 7.50006L1.18752 8.50006C1.18752 8.73674 1.38691 8.92863 1.63283 8.92863L14.3032 8.92863L8.54638 14.469C8.37249 14.6363 8.37249 14.9077 8.54638 15.0751L9.28111 15.7822C9.455 15.9496 9.73696 15.9496 9.91089 15.7822L17.6821 8.30309C17.856 8.13574 17.856 7.86438 17.6821 7.69699L9.91086 0.217952C9.73696 0.0505587 9.455 0.0505586 9.28111 0.217951Z"></path>
                           </svg>
                         </a>
+                              </Link>
                       </nav>
                     </div>
                   </div>
@@ -255,13 +257,13 @@ export default function Post({ posts }) {
                 </div>
               </div>
             </div>
-            <div className="elementor-element elementor-element-09f894c elementor-absolute e-transform elementor-widget elementor-widget-bt-pattern-background">
+{/* <div className="elementor-element elementor-element-09f894c elementor-absolute e-transform elementor-widget elementor-widget-bt-pattern-background">
               <div className="elementor-widget-container">
                 <div className="bt-elwg-pattern-background">
                   <div className="bt-bg-pattern-effect"></div>
                 </div>
               </div>
-            </div>
+            </div>*/}
           </div>
         </div>
 
@@ -269,10 +271,7 @@ export default function Post({ posts }) {
       </div>
   <div className="global-wrap">
         <div className="global-content">
-          {/*** header ***/}
-
-          <Header />
-          {/*** end ***/}
+          
 
           <main className="global-main">
        
