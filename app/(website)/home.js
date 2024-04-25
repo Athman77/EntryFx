643f2f87a29d3f86dc2e9960f418c07db355d70e
Athman77 <67540110+Athman77@users.noreply.github.com>
2024-04-25 13:23:14 +0300

    Update home.js

--- app/(website)/home.js
+++ app/(website)/home.js
@@ -1,3 +1,4 @@
+
 import Link from "next/link";
 import Container from "@/components/container";
 import PostList from "@/components/postlist";
@@ -36,6 +37,7 @@ export default function Post({ posts }) {
               </div>
             </div>
             {/**** ====== hero section end ===== ***/}
+</main>
 
         <div data-elementor-type="wp-page" data-elementor-id="4810" className="elementor elementor-4810" data-elementor-post-type="page">
     
@@ -155,22 +157,10 @@ export default function Post({ posts }) {
 
 </div> 
 
+
+<main>
   
-            <div className="featured-section items-4">
-              <div className="featured-wrap">
-                {posts &&
-                  posts
-                    .slice(0, 4)
-                    .map(post => (
-                      <PostList
-                        key={post._id}
-                        post={post}
-                        aspect="landscape"
-                        preloadImage={true}
-                      />
-                    ))}
-              </div>
-            </div>
+            
 
             {/*** blog ***/}
             <div className="loop-section">
@@ -294,30 +284,7 @@ export default function Post({ posts }) {
         </div>
       </div>
 
-      {/* <Container>
-          <div className="grid gap-10 md:grid-cols-2 lg:gap-10 ">
-            {posts.slice(0, 2).map(post => (
-              <PostList
-                key={post._id}
-                post={post}
-                aspect="landscape"
-                preloadImage={true}
-              />
-            ))}
-          </div>
-          <div className="mt-10 grid gap-10 md:grid-cols-2 lg:gap-10 xl:grid-cols-3 ">
-            {posts.slice(2, 14).map(post => (
-              <PostList key={post._id} post={post} aspect="square" />
-            ))}
-          </div>
-          <div className="mt-10 flex justify-center">
-            <Link
-              href="/archive"
-              className="relative inline-flex items-center gap-1 rounded-md border border-gray-300 bg-white px-3 py-2 pl-4 text-sm font-medium text-gray-500 hover:bg-gray-50 focus:z-20 disabled:pointer-events-none disabled:opacity-40 dark:border-gray-500 dark:bg-gray-800 dark:text-gray-300">
-              <span>View all Posts</span>
-            </Link>
-          </div>
-        </Container>*/}
+    
     </>
   );
 }
