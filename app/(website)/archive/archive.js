3cd6b024aca2f4ff2b307a403b8b3b0294461e09
Athman77 <67540110+Athman77@users.noreply.github.com>
2024-04-26 11:36:23 +0300

    h

--- app/(website)/archive/archive.js
+++ app/(website)/archive/archive.js
@@ -19,7 +19,7 @@ export default async function Post({ searchParams }) {
   // Define the parameters for fetching posts based on the current page
   const params = {
     pageIndex: (pageIndex - 1) * POSTS_PER_PAGE,
-    limit: pageIndex * POSTS_PER_PAGE
+    limit: pageIndex * POSTS_PER_PAGE,
   };
 
   const posts = await getPaginatedPosts(params);
@@ -30,46 +30,35 @@ export default async function Post({ searchParams }) {
 
   return (
     <>
-  <div className="global-cover"></div>
+      <div className="global-cover"></div>
 
       <div className="global-wrap">
         <div className="global-content">
           <Header />
-    
 
-    
- <main className="global-main">
-            
+          <main className="global-main">
+            {posts && posts?.length === 0 && (
+              <div className="flex h-40 items-center justify-center">
+                <span className="text-lg text-gray-500">
+                  End of the result!
+                </span>
+              </div>
+            )}
 
-    
-      {posts && posts?.length === 0 && (
-        <div className="flex h-40 items-center justify-center">
-          <span className="text-lg text-gray-500">
-            End of the result!
-          </span>
-        </div>
-      )}
+            {/** ==.hhd ==**/}
 
-
-       {/** ==.hhd ==**/}
-            
-                {posts && posts?.length === 0 && (
-                  <div className="flex h-40 items-center justify-center">
-                    <span className="text-lg text-gray-500">
-                      End of the result!
-                    </span>
-                  </div>
-                )}
-               
-
-
+            {posts && posts?.length === 0 && (
+              <div className="flex h-40 items-center justify-center">
+                <span className="text-lg text-gray-500">
+                  End of the result!
+                </span>
+              </div>
+            )}
           </main>
-          
-                 </div>
-                          </div>
+        </div>
+      </div>
 
-
-<div
+      <div
         data-elementor-type="wp-page"
         data-elementor-id="4810"
         className="elementor elementor-4810"
@@ -158,16 +147,16 @@ export default async function Post({ searchParams }) {
                       <div className="bt-podcast-grid">
                         {posts &&
                           posts.map((post) => (
-                              <PostList
-                                key={post._id}
-                                post={post}
-                                aspect="landscape"
-                                preloadImage={true}
-                              />
-                            ))}
+                            <PostList
+                              key={post._id}
+                              post={post}
+                              aspect="landscape"
+                              preloadImage={true}
+                            />
+                          ))}
                       </div>
 
-                       <nav className="bt-pagination" role="navigation">
+                      <nav className="bt-pagination" role="navigation">
                         {" "}
                         <span
                           aria-current="page"
@@ -179,23 +168,20 @@ export default async function Post({ searchParams }) {
                           <a className="page-numbers">2</a>{" "}
                         </Link>
                         <a className="page-numbers">3</a>{" "}
-                            <Link href="/archive">
-                        <a
-                          className="next page-numbers"
-                         
-                        >
-                          Next
-                          <svg
-                            width="19"
-                            height="16"
-                            viewBox="0 0 19 16"
-                            fill="currentColor"
-                            xmlns="http://www.w3.org/2000/svg"
-                          >
-                            <path d="M9.28111 0.217951L8.54638 0.925058C8.37249 1.09242 8.37249 1.36377 8.54638 1.53117L14.3032 7.07149L1.63283 7.07149C1.38691 7.07149 1.18752 7.26338 1.18752 7.50006L1.18752 8.50006C1.18752 8.73674 1.38691 8.92863 1.63283 8.92863L14.3032 8.92863L8.54638 14.469C8.37249 14.6363 8.37249 14.9077 8.54638 15.0751L9.28111 15.7822C9.455 15.9496 9.73696 15.9496 9.91089 15.7822L17.6821 8.30309C17.856 8.13574 17.856 7.86438 17.6821 7.69699L9.91086 0.217952C9.73696 0.0505587 9.455 0.0505586 9.28111 0.217951Z"></path>
-                          </svg>
-                        </a>
-                              </Link>
+                        <Link href="/archive">
+                          <a className="next page-numbers">
+                            Next
+                            <svg
+                              width="19"
+                              height="16"
+                              viewBox="0 0 19 16"
+                              fill="currentColor"
+                              xmlns="http://www.w3.org/2000/svg"
+                            >
+                              <path d="M9.28111 0.217951L8.54638 0.925058C8.37249 1.09242 8.37249 1.36377 8.54638 1.53117L14.3032 7.07149L1.63283 7.07149C1.38691 7.07149 1.18752 7.26338 1.18752 7.50006L1.18752 8.50006C1.18752 8.73674 1.38691 8.92863 1.63283 8.92863L14.3032 8.92863L8.54638 14.469C8.37249 14.6363 8.37249 14.9077 8.54638 15.0751L9.28111 15.7822C9.455 15.9496 9.73696 15.9496 9.91089 15.7822L17.6821 8.30309C17.856 8.13574 17.856 7.86438 17.6821 7.69699L9.91086 0.217952C9.73696 0.0505587 9.455 0.0505586 9.28111 0.217951Z"></path>
+                            </svg>
+                          </a>
+                        </Link>
                       </nav>
                     </div>
                   </div>
@@ -226,7 +212,7 @@ export default async function Post({ searchParams }) {
                 </div>
               </div>
             </div>
-{/* <div className="elementor-element elementor-element-09f894c elementor-absolute e-transform elementor-widget elementor-widget-bt-pattern-background">
+            {/* <div className="elementor-element elementor-element-09f894c elementor-absolute e-transform elementor-widget elementor-widget-bt-pattern-background">
               <div className="elementor-widget-container">
                 <div className="bt-elwg-pattern-background">
                   <div className="bt-bg-pattern-effect"></div>
@@ -239,28 +225,27 @@ export default async function Post({ searchParams }) {
         {/*--_-gaucho---**/}
       </div>
 
-            <Pagination
-        pageIndex={pageIndex}
-        isFirstPage={isFirstPage}
-        isLastPage={isLastPage}
-        tag="archive"
-      />     
-
-      <div className="global-wrap">
-        <div className="global-content">
-        <main className="global-main">
-
       <Pagination
         pageIndex={pageIndex}
         isFirstPage={isFirstPage}
         isLastPage={isLastPage}
         tag="archive"
       />
-                     <Subscribe />
+
+      <div className="global-wrap">
+        <div className="global-content">
+          <main className="global-main">
+            <Pagination
+              pageIndex={pageIndex}
+              isFirstPage={isFirstPage}
+              isLastPage={isLastPage}
+              tag="archive"
+            />
+            <Subscribe />
           </main>
           <Footer />
-                 </div>
-                          </div>
+        </div>
+      </div>
     </>
   );
 }
