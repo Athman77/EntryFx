9f5353da101d54cdae1de467d171cf2a1e6f55b7
Athman77 <67540110+Athman77@users.noreply.github.com>
2024-06-15 10:29:43 +0300

    Update home.js

--- app/(website)/home.js
+++ app/(website)/home.js
@@ -1,3 +1,4 @@
+//
 
 
 import Link from "next/link";
@@ -43,229 +44,29 @@ export default function Post({ posts }) {
       </div>
       {/*****/}
       
- 
+   <div className="bg-[#f8f8f8]   text-base dark:bg-neutral-900/95 text-neutral-900 dark:text-neutral-200">
 
-      <div
-        data-elementor-type="wp-page"
-        data-elementor-id="3718"
-        className="elementor elementor-3718"
-        data-elementor-post-type="page"
-      >
-        {/****section***/}
-        <div
-          className="elementor-element elementor-element-8511b5e e-con-full e-flex e-con e-parent"
-          data-id="8511b5e"
-          data-element_type="container"
-          data-core-v316-plus="true"
-        >
-          <div
-            className="elementor-element elementor-element-fcc57a8 e-con-full e-flex e-con e-child"
-            data-id="fcc57a8"
-            data-element_type="container"
-          >
-            <div
-              className="elementor-element elementor-element-ea875a4 bt-image-effect elementor-widget elementor-widget-bt-podcast-grid"
-              data-id="ea875a4"
-              data-element_type="widget"
-              data-widget_type="bt-podcast-grid.default"
-            >
-              <div className="elementor-widget-container">
-                <div className="bt-elwg-podcast-grid--default">
-                  <div className="bt-podcast-grid">
-                    {posts &&
-                      posts
-                        .slice(0, 5)
-                        .map((post) => (
-                          <BlogOne
-                            key={post._id}
-                            post={post}
-                            aspect="landscape"
-                            preloadImage={true}
-                          />
-                        ))}
+       <div className="nc-PageHome relative">
+            <div className="container relative">
+              <div className="nc-SectionLargeSlider relative pt-10 pb-16 md:py-16 lg:pb-28 lg:pt-20">
+
+      {/*/////content////*/}
+      <div className="nc-Section-Heading relative flex flex-col sm:flex-row sm:items-end justify-between mb-10 md:mb-12 text-neutral-900 dark:text-neutral-50">
+                  <div className="max-w-2xl">
+                    <h2 className="text-2xl md:text-3xl lg:text-4xl font-semibold">Editor's pick</h2><span className="mt-2 md:mt-3 font-normal block text-base sm:text-xl text-neutral-500 dark:text-neutral-400">Discover the most outstanding articles in all topics of life. </span>
                   </div>
                 </div>
-              </div>
-            </div>
-          </div>
-        </div>
-
-        {/****new one***/}
-      </div>
-
-      {/*/////blog section////*/}
+      
+      {/*********/}
+      
+</div>
+</div>
+</div>
+</div>
 
       {/* <!------ post card------>*/}
 
-      <div
-        data-elementor-type="wp-page"
-        data-elementor-id="4810"
-        className="elementor elementor-4810"
-        data-elementor-post-type="page"
-      >
-        {/* <!------ post card------>*/}
-        <div className="elementor-element elementor-element-510726c e-flex e-con-boxed e-con e-parent">
-          <div className="e-con-inner">
-            <div className="elementor-element elementor-element-cb5e833 elementor-absolute e-transform elementor-widget elementor-widget-bt-orbit-circle">
-              <div className="elementor-widget-container">
-                <div className="bt-elwg-orbit-circle">
-                  <div className="bt-orbit-wrap">
-                    <div className="bt-orbit red">
-                      <span></span>
-                    </div>
-                    <div className="bt-orbit blue">
-                      <span></span>
-                    </div>
-                    <div className="bt-orbit yellow">
-                      <span></span>
-                    </div>
-                    <div className="bt-orbit purple">
-                      <span></span>
-                    </div>
-                    <div className="bt-orbit green">
-                      <span></span>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div className="elementor-element elementor-element-09f894c elementor-absolute e-transform elementor-widget elementor-widget-bt-pattern-background">
-              <div className="elementor-widget-container">
-                <div className="bt-elwg-pattern-background">
-                  <div className="bt-bg-pattern-effect"></div>
-                </div>
-              </div>
-            </div>
-            <div className="elementor-element elementor-element-b28e6c6 e-flex e-con-boxed e-con e-child">
-              <div className="e-con-inner">
-                <div className="elementor-element elementor-element-170057f elementor-widget elementor-widget-heading">
-                  <div className="elementor-widget-container">
-                    <h6 className="elementor-heading-title elementor-size-default">
-                      Latest Show
-                    </h6>
-                  </div>
-                </div>
-                <div
-                  className="elementor-element elementor-element-bd103a3 elementor-widget elementor-widget-heading"
-                  data-id="bd103a3"
-                  data-element_type="widget"
-                  data-widget_type="heading.default"
-                >
-                  <div className="elementor-widget-container">
-                    <h2 className="elementor-heading-title elementor-size-default text-white">
-                      My recent <span>highlights</span>
-                    </h2>
-                  </div>
-                </div>
-                <div className="elementor-element elementor-element-8f9a284 elementor-widget__width-initial elementor-widget elementor-widget-text-editor">
-                  <div className="elementor-widget-container">
-                    <p className="text-white">
-                      Get Live Football Scores and Real-Time Football Results with LiveScore!.
-                    </p>
-                  </div>
-                </div>
-              </div>
-            </div>
-
-            <div
-              className="elementor-element elementor-element-061914d e-flex e-con-boxed e-con e-child"
-              data-id="061914d"
-              data-element_type="container"
-            >
-              <div className="e-con-inner">
-                <div
-                  className="elementor-element elementor-element-922d7a7 bt-image-effect elementor-widget elementor-widget-bt-podcast-grid-style-2"
-                  data-id="922d7a7"
-                  data-element_type="widget"
-                  data-widget_type="bt-podcast-grid-style-2.default"
-                >
-                  <div className="elementor-widget-container">
-                    <div className="bt-elwg-podcast-grid--style-2">
-                      <div className="bt-podcast-grid">
-                        {posts &&
-                          posts
-                            .slice(5, 13)
-                            .map((post) => (
-                              <PostList
-                                key={post._id}
-                                post={post}
-                                aspect="landscape"
-                                preloadImage={true}
-                              />
-                            ))}
-                      </div>
-
-                       <nav className="bt-pagination" role="navigation">
-                        {" "}
-                        <span
-                          aria-current="page"
-                          className="page-numbers current"
-                        >
-                          1
-                        </span>{" "}
-                        <Link href="/archive">
-                          <a className="page-numbers">2</a>{" "}
-                        </Link>
-                        <a className="page-numbers">3</a>{" "}
-                            <Link href="/archive">
-                        <a
-                          className="next page-numbers"
-                         
-                        >
-                          Next
-                          <svg
-                            width="19"
-                            height="16"
-                            viewBox="0 0 19 16"
-                            fill="currentColor"
-                            xmlns="http://www.w3.org/2000/svg"
-                          >
-                            <path d="M9.28111 0.217951L8.54638 0.925058C8.37249 1.09242 8.37249 1.36377 8.54638 1.53117L14.3032 7.07149L1.63283 7.07149C1.38691 7.07149 1.18752 7.26338 1.18752 7.50006L1.18752 8.50006C1.18752 8.73674 1.38691 8.92863 1.63283 8.92863L14.3032 8.92863L8.54638 14.469C8.37249 14.6363 8.37249 14.9077 8.54638 15.0751L9.28111 15.7822C9.455 15.9496 9.73696 15.9496 9.91089 15.7822L17.6821 8.30309C17.856 8.13574 17.856 7.86438 17.6821 7.69699L9.91086 0.217952C9.73696 0.0505587 9.455 0.0505586 9.28111 0.217951Z"></path>
-                          </svg>
-                        </a>
-                              </Link>
-                      </nav>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-
-            <div className="elementor-element elementor-element-cb5e833 elementor-absolute e-transform elementor-widget elementor-widget-bt-orbit-circle">
-              <div className="elementor-widget-container">
-                <div className="bt-elwg-orbit-circle">
-                  <div className="bt-orbit-wrap">
-                    <div className="bt-orbit red">
-                      <span></span>
-                    </div>
-                    <div className="bt-orbit blue">
-                      <span></span>
-                    </div>
-                    <div className="bt-orbit yellow">
-                      <span></span>
-                    </div>
-                    <div className="bt-orbit purple">
-                      <span></span>
-                    </div>
-                    <div className="bt-orbit green">
-                      <span></span>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-{/* <div className="elementor-element elementor-element-09f894c elementor-absolute e-transform elementor-widget elementor-widget-bt-pattern-background">
-              <div className="elementor-widget-container">
-                <div className="bt-elwg-pattern-background">
-                  <div className="bt-bg-pattern-effect"></div>
-                </div>
-              </div>
-            </div>*/}
-          </div>
-        </div>
-
-        {/*--_-gaucho---**/}
-      </div>
+     
   <div className="global-wrap">
         <div className="global-content">
           
