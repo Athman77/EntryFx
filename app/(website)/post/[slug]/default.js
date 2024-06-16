24c75e1e083c04cd0cb7ee2d9c82dcb7624a95f1
Athman77 <67540110+Athman77@users.noreply.github.com>
2024-06-16 15:31:00 +0300

    Update default.js

--- app/(website)/post/[slug]/default.js
+++ app/(website)/post/[slug]/default.js
@@ -38,10 +38,10 @@ export default function Post(props) {
   return (
     <>
 
-
+<div className="global-cover"></div>
 
      <div className="bg-[#f8f8f8] text-base dark:bg-neutral-900/95 text-neutral-900 dark:text-neutral-200 font-body">
-    <div className="global-cover"></div>
+    
     {/**** ***/}
      <div className="nc-PageSingle pt-8 lg:pt-16">
       <header className="container rounded-xl">
