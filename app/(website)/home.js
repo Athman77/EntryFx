7061b9dd62f4f4f6a738e1cfbab06683e6f15594
Athman77 <67540110+Athman77@users.noreply.github.com>
2024-07-25 19:52:04 +0300

    Update home.js

--- app/(website)/home.js
+++ app/(website)/home.js
@@ -90,7 +90,7 @@ export default function Post({ posts }) {
                     <svg
                       className="epcl-waves"
                       xmlns="http://www.w3.org/2000/svg"
-                      xmlns:xlink="http://www.w3.org/1999/xlink"
+                     // xmlns:xlink="http://www.w3.org/1999/xlink"
                       viewBox="0 24 150 28"
                       preserveAspectRatio="none"
                       shape-rendering="auto"
