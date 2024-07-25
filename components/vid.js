a844961d3d0e23708f510e6518f97b545409fcc5
Athman77 <67540110+Athman77@users.noreply.github.com>
2024-07-25 16:50:34 +0300

    Add files via upload

---
+++ components/vid.js
@@ -0,0 +1,29 @@
+//
+"use client";
+import React, { useState } from "react";
+
+import ReactPlayer from "react-player";
+
+//import videoImg from 'assets/video.png';
+
+const Vid = ({ id }) => {
+  const [videoOpen, setVideoOpen] = useState(false);
+  const handleClick = e => {
+    e.preventDefault();
+    setVideoOpen(true);
+  };
+  return (
+    <div className="">
+      <ReactPlayer
+        url={`https://www.youtube.com/watch?v=${id}`}
+        className="object-cover object-cover absolute inset-0 w-full h-full"
+            
+        controls
+        width="100%"
+        height="56.25vw"
+      />
+    </div>
+  );
+};
+
+export default Vid;
