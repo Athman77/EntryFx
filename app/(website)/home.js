1897ac63b9d37cf3fa964c790d05084296daa35c
Athman77 <67540110+Athman77@users.noreply.github.com>
2024-07-25 14:54:54 +0300

    Update home.js

--- app/(website)/home.js
+++ app/(website)/home.js
@@ -13,7 +13,39 @@ import Header from "@/components/header";
 export default function Post({ posts }) {
   return (
     <>
-    <div className="bg-[#f8f8f8] text-base darkG:bg-neutral-900/95G text-neutral-900 dark:text-neutral-200 font-body">
+  <nav className="mobile main-nav hide-on-desktop-lg">
+    <ul className="menu">
+      <li className=" nav-home current-menu-item"><a href="https://ghost.estudiopatagon.com/groovy/">Home</a></li>
+      <li className=" nav-membership"><a href="https://ghost.estudiopatagon.com/groovy/membership/">Membership</a></li>
+      <li className=" nav-style-guide"><a href="https://ghost.estudiopatagon.com/groovy/style-guide/">Style Guide ✨</a></li>
+      <li className=" nav-tag"><a href="https://ghost.estudiopatagon.com/groovy/tag/getting-started/">#Tag</a></li>
+
+      <li className="nav-more menu-item-has-children">
+        <a href="javascript:void(0)">More</a>
+        <ul className="sub-menu border-left-effect">
+          <li><a href="https://ghost.estudiopatagon.com/groovy/masonry/">Home - Masonry</a></li>
+          <li><a href="https://ghost.estudiopatagon.com/groovy/standard/">Vertical Post (default)</a></li>
+          <li><a href="https://ghost.estudiopatagon.com/groovy/classNameic/">classNameic Post</a></li>
+          <li><a href="https://ghost.estudiopatagon.com/groovy/fullwidth/">Fullwidth Post</a></li>
+          <li><a href="https://ghost.estudiopatagon.com/groovy/nosidebar/">No Sidebar Post</a></li>
+          <li><a href="https://ghost.estudiopatagon.com/groovy/apps-integrations/">Text Only Post</a></li>
+          <li><a href="https://ghost.estudiopatagon.com/groovy/authors/">Meet the Authors</a></li>
+          <li><a href="https://ghost.estudiopatagon.com/groovy/author/jonathan-doe/">Single Author Page</a></li>
+          <li><a href="https://ghost.estudiopatagon.com/groovy/404/">404 Page</a></li>
+          <li><a href="https://themes.estudiopatagon.com/selector/groovy-ghost/">Home Demos 🚀</a></li>
+        </ul>
+      </li>
+
+      <li className="hide-on-mobile hide-on-tablet hide-on-desktop-sm"><a href="javascript:void(0)" data-ghost-search=""><i className="fa fa-search"></i></a></li>
+
+      <li className="nav-login"><a href="https://ghost.estudiopatagon.com/groovy/signin" className="login epcl-button dark" data-title="Sign In">Sign In</a></li>
+    </ul>
+
+
+  </nav>
+
+    
+    {/*  <div className="bg-[#f8f8f8] text-base darkG:bg-neutral-900/95G text-neutral-900 dark:text-neutral-200 font-body">
          <div className="container ">
    <div className="nc-SectionMagazine8 relative py-16 lg:py-28">
      <div className="nc-Section-Heading relative flex flex-col sm:flex-row sm:items-end justify-between mb-14 text-neutral-900 darkG:text-neutral-50">
@@ -37,7 +69,7 @@ export default function Post({ posts }) {
                 </div>
                 </div>
                 
-      
+      */}
                   
 
       {/* <!------ post card------>*/}
