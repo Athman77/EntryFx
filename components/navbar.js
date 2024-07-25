449d6107d8636b1c0f27b4497ab44759c98720a3
Athman77 <67540110+Athman77@users.noreply.github.com>
2024-07-25 21:25:37 +0300

    Update navbar.js

--- components/navbar.js
+++ components/navbar.js
@@ -1,3 +1,4 @@
+
 "use client";
 
 import { Fragment } from "react";
@@ -47,145 +48,89 @@ export default function Navbar(props) {
   const mobilemenu = [...leftmenu, ...rightmenu];
 
   return (
-    <Container>
-      <nav>
-        <Disclosure>
-          {({ open }) => (
-            <>
-              <div className="flex flex-wrap justify-between md:flex-nowrap md:gap-10">
-                <div className="order-1 hidden w-full flex-col items-center justify-start md:order-none md:flex md:w-auto md:flex-1 md:flex-row md:justify-end">
-                  {leftmenu.map((item, index) => (
-                    <Fragment key={`${item.label}${index}`}>
-                      {item.children && item.children.length > 0 ? (
-                        <DropdownMenu
-                          menu={item}
-                          key={`${item.label}${index}`}
-                          items={item.children}
-                        />
-                      ) : (
-                        <Link
-                          href={item.href}
-                          key={`${item.label}${index}`}
-                          className="px-5 py-2 text-sm font-medium text-gray-600 hover:text-blue-500 dark:text-gray-400"
-                          target={item.external ? "_blank" : ""}
-                          rel={item.external ? "noopener" : ""}>
-                          {item.label}
-                        </Link>
-                      )}
-                    </Fragment>
-                  ))}
-                </div>
-                <div className="flex w-full items-center justify-between md:w-auto">
-                  <Link href="/" className="w-28 dark:hidden">
-                    {props.logo ? (
-                      <Image
-                        {...urlForImage(props.logo)}
-                        alt="Logo"
-                        priority={true}
-                        sizes="(max-width: 640px) 100vw, 200px"
-                      />
-                    ) : (
-                      <span className="block text-center">
-                        Stablo
-                      </span>
-                    )}
-                  </Link>
-                  <Link href="/" className="hidden w-28 dark:block">
-                    {props.logoalt ? (
-                      <Image
-                        {...urlForImage(props.logoalt)}
-                        alt="Logo"
-                        priority={true}
-                        sizes="(max-width: 640px) 100vw, 200px"
-                      />
-                    ) : (
-                      <span className="block text-center">
-                        Stablo
-                      </span>
-                    )}
-                  </Link>
-                  <Disclosure.Button
-                    aria-label="Toggle Menu"
-                    className="ml-auto rounded-md px-2 py-1 text-gray-500 focus:text-blue-500 focus:outline-none dark:text-gray-300 md:hidden ">
-                    <svg
-                      className="h-6 w-6 fill-current"
-                      xmlns="http://www.w3.org/2000/svg"
-                      viewBox="0 0 24 24">
-                      {open && (
-                        <path
-                          fillRule="evenodd"
-                          clipRule="evenodd"
-                          d="M18.278 16.864a1 1 0 0 1-1.414 1.414l-4.829-4.828-4.828 4.828a1 1 0 0 1-1.414-1.414l4.828-4.829-4.828-4.828a1 1 0 0 1 1.414-1.414l4.829 4.828 4.828-4.828a1 1 0 1 1 1.414 1.414l-4.828 4.829 4.828 4.828z"
-                        />
-                      )}
-                      {!open && (
-                        <path
-                          fillRule="evenodd"
-                          d="M4 5h16a1 1 0 0 1 0 2H4a1 1 0 1 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2z"
-                        />
-                      )}
-                    </svg>
-                  </Disclosure.Button>
-                </div>
+    <header id="header" className="minimalist">
 
-                <div className="order-2 hidden w-full flex-col items-center justify-start md:order-none md:flex md:w-auto md:flex-1 md:flex-row">
-                  {rightmenu.map((item, index) => (
-                    <Fragment key={`${item.label}${index}`}>
-                      {item.children && item.children.length > 0 ? (
-                        <DropdownMenu
-                          menu={item}
-                          key={`${item.label}${index}`}
-                          items={item.children}
-                        />
-                      ) : (
-                        <Link
-                          href={item.href}
-                          key={`${item.label}${index}`}
-                          className="px-5 py-2 text-sm font-medium text-gray-600 hover:text-blue-500 dark:text-gray-400"
-                          target={item.external ? "_blank" : ""}
-                          rel={item.external ? "noopener" : ""}>
-                          <span> {item.label}</span>
-                          {item.badge && (
-                            <span className="ml-2 rounded bg-blue-100 px-2 py-0.5 text-xs font-semibold text-blue-600 dark:bg-cyan-200 dark:text-blue-800 ">
-                              {item.badge}
-                            </span>
-                          )}
-                        </Link>
-                      )}
-                    </Fragment>
-                  ))}
-                </div>
-              </div>
-              <Disclosure.Panel>
-                <div className="order-2 -ml-4 mt-4 flex w-full flex-col items-center justify-start md:hidden">
-                  {mobilemenu.map((item, index) => (
-                    <Fragment key={`${item.label}${index}`}>
-                      {item.children && item.children.length > 0 ? (
-                        <DropdownMenu
-                          menu={item}
-                          key={`${item.label}${index}`}
-                          items={item.children}
-                          mobile={true}
-                        />
-                      ) : (
-                        <Link
-                          href={item.href}
-                          key={`${item.label}${index}`}
-                          className="w-full px-5 py-2 text-sm font-medium text-gray-600 hover:text-blue-500 dark:text-gray-400"
-                          target={item.external ? "_blank" : ""}
-                          rel={item.external ? "noopener" : ""}>
-                          {item.label}
-                        </Link>
-                      )}
-                    </Fragment>
-                  ))}
-                </div>
-              </Disclosure.Panel>
-            </>
-          )}
-        </Disclosure>
-      </nav>
-    </Container>
+    <div className="menu-mobile">
+      <svg className="icon ularge open">
+        <use xlink:href="#menu-icon"></use>
+      </svg>
+      <svg className="icon ularge close">
+    
+      </svg>
+    </div>
+
+    
+    <div className="menu-wrapper">
+
+      <div className="grid-container">
+        <div className="epcl-flex">
+          <div className="logo"><a href="https://ghost.estudiopatagon.com/wavy"><img src="images/logo-wave7.svg" alt="Wavy" width="180" height="60"></a></div>
+
+          <span data-ghost-search="" className="epcl-search-button epcl-button gradient-button circle"><svg className="icon large">
+             
+            </svg></span>
+
+          
+          <nav className="main-nav">
+            <ul className="menu underline-effect">
+              <li className=" nav-home"><a href="https://ghost.estudiopatagon.com/wavy/">Home</a></li>
+              <li className=" nav-tag"><a href="https://ghost.estudiopatagon.com/wavy/tag/design/">Tag</a></li>
+              <li className=" nav-style-guide"><a href="https://ghost.estudiopatagon.com/wavy/style-guide/">Style Guide</a></li>
+              <li className=" nav-membership"><a href="https://ghost.estudiopatagon.com/wavy/membership/">Membership</a></li>
+
+              <li className="nav-more menu-item-has-children">
+                <span className="link">More</span>
+                <ul className="sub-menu">
+                  <li className=""><a href="https://ghost.estudiopatagon.com/wavy/masonry/">Home Grid Posts</a></li>
+                  <li className=""><a href="https://ghost.estudiopatagon.com/wavy/cta-masonry/">Home Grid w/ Intro</a></li>
+                  <li className=""><a href="https://ghost.estudiopatagon.com/wavy/cta-slider/">Home Slider w/ Intro</a></li>
+                  <li className=" current-menu-item"><a href="https://ghost.estudiopatagon.com/wavy/cta-classNameic/">Home classNameic w/ Intro</a></li>
+                  <li className=""><a href="https://ghost.estudiopatagon.com/wavy/vertical/">Post Vertical</a></li>
+                  <li className=""><a href="https://ghost.estudiopatagon.com/wavy/nosidebar/">Post without Sidebar</a></li>
+                  <li className=""><a href="https://ghost.estudiopatagon.com/wavy/classNameic/">Post classNameic</a></li>
+                  <li className=""><a href="https://ghost.estudiopatagon.com/wavy/fullwidth/">Post Fullwidth</a></li>
+                  <li className=""><a href="https://ghost.estudiopatagon.com/wavy/author/jonathan-doe/">Author</a></li>
+                  <li className=""><a href="https://ghost.estudiopatagon.com/wavy/meet-the-authors/">Meet the Authors</a></li>
+                  <li className=""><a href="https://ghost.estudiopatagon.com/wavy/explore-our-tags/">Tags Archive</a></li>
+                  <li className=""><a href="https://ghost.estudiopatagon.com/wavy/404/">404 Page</a></li>
+                </ul>
+              </li>
+
+              <li className="search-menu-item hide-on-mobile hide-on-tablet hide-on-desktop-sm"><span className="link" data-ghost-search=""><svg className="icon black large">
+                    <use xlink:href="#search-icon"></use>
+                  </svg> Search</span></li>
+
+            </ul>
+
+
+          </nav>
+          
+
+          <div className="account underline-effect hide-on-mobile hide-on-tablet hide-on-desktop-sm">
+            <a href="https://ghost.estudiopatagon.com/wavy/signin" className="link-button epcl-login">
+              Login
+            </a>
+            <a href="https://ghost.estudiopatagon.com/wavy/subscribe" className="epcl-button gradient-button icon wave-button">
+              <svg className="icon large">
+              
+              </svg>
+              Subscribe
+            </a>
+          </div>
+
+          <div className="clear"></div>
+
+        </div>
+
+        <div className="clear"></div>
+      </div>
+
+    </div>
+    
+
+    <div className="clear"></div>
+
+  </header>
   );
 }
 
