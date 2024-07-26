30d1ec4b8730ec6dda880a16e6c9699e288716b2
Athman77 <67540110+Athman77@users.noreply.github.com>
2024-07-27 00:38:46 +0300

    Update navbar.js

--- components/navbar.js
+++ components/navbar.js
@@ -49,39 +49,62 @@ export default function Navbar(props) {
   const mobilemenu = [...leftmenu, ...rightmenu];
 
   return (
-     <header className="header-section">
-             <div className="header-wrap">
-               <div className="header-logo">
-                 <a href="https://forest.quest" className="is-image"><img src="images/Brandmark-design--5-..png" alt="The Forest" /></a>
-               </div>
-               <div className="header-nav">
-                 <input id="toggle" className="header-checkbox" type="checkbox" />
-                 <label className="header-toggle" for="toggle">
-                   <span>
-                     <span className="bar"></span>
-                     <span className="bar"></span>
-                     <span className="bar"></span>
-                   </span>
-                 </label>
-                 <nav>
-     
-                   <ul>
-                     <li><a href="https://forest.quest/manifesto/">Manifesto</a></li>
-                     <li><a href="https://forest.quest/foundations/">Foundations</a></li>
-                     <li><a href="https://forest.quest/coaching/">Coaching</a></li>
-                   </ul>
-     
-                   <ul>
-                     <li className="signin"><a href="https://forest.quest/signin/">Sign in</a></li>
-                     <li className="signup"><a href="https://forest.quest/purchase/" className="global-button">Purchase</a></li>
-                   </ul>
-                   <span data-ghost-search=""><svg role="img" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                       <path d="M16.877 18.456l5.01 5.011c.208.197.484.308.771.308a1.118 1.118 0 00.809-1.888l-5.011-5.01c3.233-4.022 2.983-9.923-.746-13.654l-.291-.29a.403.403 0 00-.095-.075C13.307-.77 7.095-.649 3.223 3.223c-3.997 3.998-3.997 10.489 0 14.485 3.731 3.731 9.633 3.981 13.654.748zm-.784-13.617a7.96 7.96 0 010 11.254 7.961 7.961 0 01-11.253 0 7.96 7.96 0 010-11.254 7.961 7.961 0 0111.253 0z"></path>
-                     </svg></span>
-                 </nav>
-               </div>
-             </div>
-           </header>
+ <header className="site-header d-flex">
+   <div className="container-fluid d-flex justify-content-between align-items-center">
+     <div className="logo">
+       <a href="https://advant.gbjsolution.com" className="logo-image" id="logo">
+         <img src="images/logo.svg" alt="Advant" />
+       </a>
+       <a href="https://advant.gbjsolution.com" className="logo-image" id="logo-alternete" >
+         <img src="images/logo-alternate.svg" alt="Advant" />
+       </a>
+     </div>
+     <nav className="nav-wrap d-none d-lg-block">
+       <ul className="nav-list">
+         <li className="home nav-item nav-item-current"><a href="https://advant.gbjsolution.com/">Home</a></li>
+         <li className="features nav-item"><a href="https://advant.gbjsolution.com/features/">Features</a></li>
+         <li className="style-guide nav-item"><a href="https://advant.gbjsolution.com/style-guide/">Style Guide</a></li>
+         <li className="tags nav-item"><a href="https://advant.gbjsolution.com/tag-archive/">Tags</a></li>
+         <li className="authors nav-item"><a href="https://advant.gbjsolution.com/author-archive/">Authors</a></li>
+         <li className="contact nav-item"><a href="https://advant.gbjsolution.com/contact/">Contact</a></li>
+         <li className="error-404 nav-item"><a href="https://advant.gbjsolution.com/error-404/">Error 404</a></li>
+         <li className="nav-item "><a href="/signin/">Sign in</a></li>
+         <li className="nav-item "><a href="/signup/">Sign up</a></li>
+       </ul>
+
+     </nav>
+     <div className="nav-right">
+       <input type="checkbox" name="theme-toggle" id="switch_theme" className="theme-toggle" />
+       <label for="switch_theme" className="theme-toggle-label fade-in">Toggle Theme</label>
+       <a href="#" className="search-icon js-search-button" aria-label="Search" tabindex="0"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+           <path d="M13.858,15.283a8.57,8.57,0,1,1,1.425-1.425L24,22.576,22.576,24ZM2.014,8.561A6.547,6.547,0,1,0,8.561,2.014,6.554,6.554,0,0,0,2.014,8.561Z"></path>
+         </svg></a>
+       <div className="menu-icon d-lg-none js-menu-open">
+         <span className="line"></span>
+         <span className="line"></span>
+         <span className="line"></span>
+       </div>
+     </div>
+     <nav className="mobile-menu nav-wrap d-lg-none" id="mobile-menu">
+       <ul className="nav-list">
+         <ul className="nav-list">
+           <li className="home nav-item nav-item-current"><a href="https://advant.gbjsolution.com/">Home</a></li>
+           <li className="features nav-item"><a href="https://advant.gbjsolution.com/features/">Features</a></li>
+           <li className="style-guide nav-item"><a href="https://advant.gbjsolution.com/style-guide/">Style Guide</a></li>
+           <li className="tags nav-item"><a href="https://advant.gbjsolution.com/tag-archive/">Tags</a></li>
+           <li className="authors nav-item"><a href="https://advant.gbjsolution.com/author-archive/">Authors</a></li>
+           <li className="contact nav-item"><a href="https://advant.gbjsolution.com/contact/">Contact</a></li>
+           <li className="error-404 nav-item"><a href="https://advant.gbjsolution.com/error-404/">Error 404</a></li>
+           <li className="nav-item "><a href="/signin/">Sign in</a></li>
+           <li className="nav-item "><a href="/signup/">Sign up</a></li>
+         </ul>
+       </ul>
+     </nav>
+     <div className="backdrop d-lg-none js-backdrop" id="backdrop">
+     </div>
+   </div>
+ </header>
+
   );
 }
 
