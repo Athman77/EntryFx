c772f0ddf3e4ac09c38159215575587e40260058
Athman77 <67540110+Athman77@users.noreply.github.com>
2024-07-27 00:19:05 +0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@
 import "@/styles/screen.css";
 import "@/styles/cards.min.css";
 //import "@/styles/sportx.css";
-import "@/styles/sport.css";
+//import "@/styles/sport.css";
 //import "@/styles/bottom.css";
 import { Analytics } from '@vercel/analytics/react';
 import { Providers } from "./providers";
