638b7f7c695d44cfc27a6da09637e8106e13024c
Athman77 <67540110+Athman77@users.noreply.github.com>
2024-07-27 00:57:39 +0300

    Update blogone.js

--- components/blogone.js
+++ components/blogone.js
@@ -1,4 +1,3 @@
-//
 
 import Image from "next/image";
 import Link from "next/link";
@@ -20,64 +19,59 @@ export default function BlogOne({
   fontWeight,
 }) {
   const imageProps = post?.mainImage ? urlForImage(post.mainImage) : null;
-  const AuthorimageProps = post?.author?.image
-    ? urlForImage(post.author.image)
-    : null;
+  const AuthorimageProps = post?.author?.image ?
+    urlForImage(post.author.image) :
+    null;
   return (
     <>
-        <article className="large-article index-0  post tag-reviews tag-hash-import-2023-11-22-03-40">
-    	                                                <h1 className="title medium"><a href="/airo/classNameic/">What are airports going to do about drones in the future?</a></h1>
-    	                               
-    	                                                <div className="featured-image hover-effect grid-50 tablet-grid-50 grid-parent">
-    	                               
-    	                               <a href="/airo/classNameic/" className="thumb cover">
-    	                        {post.videoType === "upload" ? (
+    
+      <article className="post tag-lifestyle tag-travel tag-education fade-in-up">
+          <div className="container-fluid">
+            <div className="row">
+              <div className="col-md-6">
+                <div className="post-thumbnail-wrap">
+                  <a href="/riding-bicycle-is-environment-friendly-and-good-for-health/" aria-label="Riding bicycle is environment friendly and good for health" tabindex="-1">
+                  {post.videoType === "upload" ? (
                 <video
-                  className="img cover"
+                  className="img post-thumbnail"
                   
                   src={post?.videoUrl}
                 />
               ) : (
                 <Vid
-                  className="img cover"
+                  className="img 
+                 post-thumbnail"
                   id={post.youtube || "Thumbnail"}
                 />
               )}   
-</a>
-{/* <a href="/airo/classNameic/" className="thumb cover"
-    	                                         //style="background: url(images/samuel-schwendener-tF2rITQWkNY-unsplash.jpg) no-repeat;"
-    	                                         ></a>*/}
-    	                                                </div>
-    	                               
-    	                                                <div className="excerpt grid-50 tablet-grid-50 grid-parent">
-    	                                                  For the second time in less than a month, flights at a major London airport have been...
-    	                                                  <div className="clear"></div>
-    	                                                  <div className="meta">
-    	                                                    <a href="/airo/author/estudio/" className="author-meta" title="Estudio Patagon">
-    	                                                      <span className="author-image cover"
-    	                                          //style="background-image: url( https://www.gravatar.com/avatar/26263d3127afcbc9393ba1d98bb510f7?s=250&amp;r=x&amp;d=mp );"
-    	                                          ></span>
-    	                                                      <span className="author-name">Estudio Patagon</span>
-    	                                                    </a>
-    	                                                    <a href="/airo/classNameic/#comments" className="comments meta-info alignright"><i className="remixicon remixicon-chat-2-line"></i> <span className="disqus-comment-count" data-disqus-url="https://ghost.estudiopatagon.com/airo/airo/classNameic/" data-disqus-identifier="5d39fcde48d02a0592b82fb5">0</span></a>
-    	                                                    <div className="clear"></div>
-    	                                                  </div>
-    	                                                  <a href="/airo/classNameic/" className="button small">Read More</a>
-    	                                                </div>
-    	                               
-    	                                                <div className="clear"></div>
-    	                               
-    	                                              </article>
+                  
+                   {/* <img className="post-thumbnail" src="" alt="Riding bicycle is environment friendly and good for health" />
+                    */}
+                  </a>
+                </div>
+              </div>
+       
+              <div className="col-md-6">
+                <div className="post-container">
+                  <time className="date" datetime="2019-07-22 07:14">22 July, 2019</time>
+                  <span className="watermark" aria-hidden="true"></span>
+                  <div className="primary-tag">
+                    <a href="/tag/lifestyle/">Lifestyle</a>
+                  </div>
+                  <h2 className="post-title h1"><a href="/riding-bicycle-is-environment-friendly-and-good-for-health/">Riding bicycle is environment friendly and good for health</a></h2>
+                  <div className="separetor"></div>
+                  <div className="reading-time">3 min read</div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </article>
     
     
     
     
     
-    
-    
-    
-    
-   
+  
     </>
   );
 }
