bd538992c358a7ec47a6814fdf5bd3f08f29db6a
Athman77 <67540110+Athman77@users.noreply.github.com>
2025-03-28 14:57:47 +0300

    Update default.js

--- app/(website)/post/[slug]/default.js
+++ app/(website)/post/[slug]/default.js
@@ -1,3 +1,4 @@
+//
 
 import Image from "next/image";
 import Link from "next/link";
@@ -42,6 +43,7 @@ export default function Post(props) {
               <div className="col">
                 <div className="post-wrap">
                   <article className="post-single post tag-lifestyle tag-travel tag-education">
+                  
                     <div className="post-header bg-post d-flex has-image">
                       <div className="header-content-wrap">
                         <div className="primary-tag text-center">
@@ -59,6 +61,50 @@ export default function Post(props) {
                         </div>
                       </div>
                     </div>
+                    {/***** YouTube****/}
+                         <div className="post-header bg-post d-flex has-image">
+                      <div className="header-content-wrap">
+                    
+                       {post.videoType === "upload" ? (
+                            <video
+                              className="rounded-xl object-cover bg-dark/10 absolute h-full w-full left-0 top-0 right-0 bottom-0 text-transparent"
+                              id="my-video"
+                              controls
+                              poster={cover?.src}
+                              // autoPlay
+                              playsInline>
+                              <source
+                                src={post?.videoUrl}
+                                type="video/mp4"
+                                data-quality="720p"
+                                title="720p"
+                                label="720p"
+                                res="720"
+                                size="720"
+                              />
+                            </video>
+                          ) : post.videoType === "youtube" ? (
+                            <VideoOne id={post.youtube || "Thumbnail"} />
+                          ) : (
+                            <>
+                              {imageProps && (
+                                <img
+                                  sizes="(max-width:480px) 300px, (max-width:768px) 600px, (max-width:1024px) 1200px, 2000px"
+                                  className="rounded-xl object-cover bg-dark/10  absolute h-full w-full left-0 top-0 right-0 bottom-0 text-transparent"
+                                  src={imageProps.src}
+                                  alt={
+                                    post.mainImage?.alt || "Thumbnail"
+                                  }
+                                />
+                              )}
+                            </>
+                          )}
+                  
+                        
+                      </div>
+                    </div>
+                    
+                    {/******/}
                     <div className="post-content">
                       {post.body && <PortableText value={post.body} />}
                     </div>
