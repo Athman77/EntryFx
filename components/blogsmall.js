0cf380c6b272d7da44de46783b349baf8b89ad1a
Athman77 <67540110+Athman77@users.noreply.github.com>
2025-07-20 17:03:00 +0300

    Update blogsmall.js

--- components/blogsmall.js
+++ components/blogsmall.js
@@ -1,4 +1,5 @@
 //
+//
 
 import Image from "next/image";
 import Link from "next/link";
@@ -26,57 +27,40 @@ export default function BlogSmall({
     null;
   return (
     <>
-      <article className="post-card">
-        <Link
+     <article className="post-card post-card-small">
+       <Link
                   href={`/post/${pathPrefix ? `${pathPrefix}/` : ""}${
                     post.slug.current
                   }`}
                 >
-        <a className="post-img-wrap">
-               {imageProps ? (
-                     <img loading="lazy" sizes="(max-width:432px) 400px, (max-width:696px) 600px, (max-width:767px) 671px, 400px" src={imageProps.src} alt="s" />
+  <a>
+    {imageProps ? (
+                     <img loading="lazy"  src={imageProps.src} alt="s" />
                     ) : (
                       <span classNameName="absolute left-1/2 top-1/2 h-16 w-16 -translate-x-1/2 -translate-y-1/2 text-gray-200">
                         <PhotoIcon />
                       </span>
                     )}
-                
-                                    </a>
-              </Link>
-                                    <div className="post-info-wrap">
-                                        <h2 className="h4 post-title">
-           <Link
+  </a>
+  </Link>
+  <div className="post-info-wrap">
+    <h2 className="h5 post-title">
+      <Link
                   href={`/post/${pathPrefix ? `${pathPrefix}/` : ""}${
                     post.slug.current
                   }`}
-                >       
+                >
       <a>{post.title}</a>
       </Link>
-                                        </h2>
-                                        <div className="post-meta text-s flex align-center">
-                                            <div className="author-avatars flex">
-         <a href="/" className="author-image-wrap" title="Harini Banerjee">
-      { post?.author?.image && ( 
-       <img src = { AuthorimageProps.src } alt = { post?.author?.name } />
-     )
-   }
-                                                </a>
-                                            </div>
-                                            <div className="author-names">
-         <a href="/">{post?.author?.name}</a>
-                                            </div>
-                                            <span className="read-time">2 min read</span>
-                                            <span className="comment-count">
-                                                <a href="" className="flex align-center #i-comment"><svg>
-                                                        {/*<use xlink:href="#i-comment"></use>*/}
-                                                    </svg>
-                                                    
-                                                </a>
-                                            </span>
-                                        </div>
-                                    </div>
-                                </article>
-                              
+    </h2>
+  </div>
+</article> 
+      
+      
+      
+      
+      
+      
    
     </>
   );
