2a06981ceda901e7e623997515e21e29570b47d2
Athman77 <67540110+Athman77@users.noreply.github.com>
2025-07-20 17:41:45 +0300

    Update blogbig.js

--- components/blogbig.js
+++ components/blogbig.js
@@ -1,6 +1,7 @@
 //
 //
 //
+//
 
 import Image from "next/image";
 import Link from "next/link";
@@ -28,41 +29,66 @@ export default function BlogBig({
     null;
   return (
     <>
-     <article className="post-card post-card-small">
-       <Link
+      
+    <article className="post-card post-card-overlapped">
+        <Link
                   href={`/post/${pathPrefix ? `${pathPrefix}/` : ""}${
                     post.slug.current
                   }`}
                 >
-  <a>
-    {imageProps ? (
-                     <img loading="lazy"  src={imageProps.src} alt="s" />
+     <a className="post-img-wrap" aria-label="featured image placeholder">
+        {imageProps ? (
+                     <img loading="lazy" 
+                     sizes="(max-width:432px) 400px, (max-width:696px) 600px, (max-width:767px) 671px, 600px"
+                     src={imageProps.src} alt="s" />
                     ) : (
                       <span classNameName="absolute left-1/2 top-1/2 h-16 w-16 -translate-x-1/2 -translate-y-1/2 text-gray-200">
                         <PhotoIcon />
                       </span>
                     )}
-  </a>
-  </Link>
-  <div className="post-info-wrap">
-    <h2 className="h5 post-title">
-      <Link
+   
+                            </a>
+                            </Link>
+                            <div className="post-info-wrap">
+                                <div className="tag-list flex">
+                                    <span className="post-visibility" data-tooltip="members">
+                                        <svg>
+                                           {/* <use xlink:href="#i-lock"></use>*/}
+                                        </svg>
+                                    </span>
+                                    <a href="/tag/health/"><span>Forex</span></a>
+                                </div>
+                                <h2 className="h4 post-title">
+                        
+          <Link
                   href={`/post/${pathPrefix ? `${pathPrefix}/` : ""}${
                     post.slug.current
                   }`}
-                >
-      <a>{post.title}</a>
-      </Link>
-    </h2>
-  </div>
-</article> 
+                >                
+  <a>{post.title}</a></Link>
+                                </h2>
+                                <div className="post-meta text-s flex align-center">
+                                    <div className="author-avatars flex">
+                                        <a href="/" className="author-image-wrap" title="Harini Banerjee">
+                                            { post?.author?.image && ( 
+       <img src = { AuthorimageProps.src } alt = { post?.author?.name } />
+     )
+   }                  </a>
+                                    </div>
+                                    <div className="author-names">
+                                        <a href="/author/harini/">{post?.author?.name}</a>
+                                    </div>
+                                    <span className="read-time">2 min read</span>
+                                </div>
+                            </div>
+                        </article>   
       
       
       
       
       
       
-   
+     
     </>
   );
 }
