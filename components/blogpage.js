6448903aa826a9c4a17c72f36be69d2914d58477
Athman77 <67540110+Athman77@users.noreply.github.com>
2025-07-20 16:43:49 +0300

    Update blogpage.js

--- components/blogpage.js
+++ components/blogpage.js
@@ -1,3 +1,4 @@
+//
 
 import Image from "next/image";
 import Link from "next/link";
@@ -11,74 +12,57 @@ export default function BlogPages() {
 
   return (
     <>
-      <div className="relative py-16">
-            <div className="absolute inset-y-0 w-screen xl:max-w-[1340px] 2xl:max-w-screen-2xl left-1/2 transform -translate-x-1/2 xl:rounded-[40px] z-0 bg-neutral-100 dark:bg-black dark:bg-opacity-20">
-             <span className="sr-only hidden">bg</span>
-            </div>
-            <div className="nc-SectionGridCategoryBox relative ">
-             <div className="nc-Section-Heading relative flex flex-col sm:flex-row sm:items-end justify-between mb-10 md:mb-12 text-neutral-900 dark:text-neutral-50">
-              <div className="text-center w-full max-w-2xl mx-auto ">
-               <h2 className="text-2xl text-neutral-900 md:text-3xl lg:text-4xl font-semibold">Top trending topics</h2>
-               <span className="mt-2 md:mt-3 font-normal block text-base sm:text-xl text-neutral-500 dark:text-neutral-400">Discover over 100 topics</span>
-              </div>
-             </div>
-             <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4 sm:gap-6 md:gap-8">
-               
-            
-              <a className="nc-CardCategory2 relative flex flex-col items-center justify-center text-center px-3 py-5 sm:p-6 bg-white dark:bg-neutral-900 rounded-3xl transition-colors " href="/archive/the-demo-archive-slug">
-               <div className="relative flex-shrink-0 w-20 h-20 rounded-full shadow-lg overflow-hidden z-0">
-                <img src="https://cdn.sanity.io/images/wusctyxy/production/df8086457a8ac050bc8bcbe1e913e17fc89990ab-736x736.jpg" alt="categories" sizes="80px" className="object-cover  object-cover absolute inset-0 w-full h-full" />
-               </div>
-               <div className="mt-3">
-                <h2 className="text-base text-neutral-900 font-semibold">Liverpool</h2>
-                <span className="block mt-1 text-sm text-neutral-500 dark:text-neutral-400">25 Articles</span>
-               </div></a>
-                <a className="nc-CardCategory2 relative flex flex-col items-center justify-center text-center px-3 py-5 sm:p-6 bg-white dark:bg-neutral-900 rounded-3xl transition-colors " href="/archive/the-demo-archive-slug">
-               <div className="relative flex-shrink-0 w-20 h-20 rounded-full shadow-lg overflow-hidden z-0">
-                <img src="https://cdn.sanity.io/images/wusctyxy/production/a1ec28f1e65f4050d5f8ccd3e3da11d094849695-387x689.jpg" alt="categories" sizes="80px" className="object-cover  object-cover absolute inset-0 w-full h-full" />
-               </div>
-               <div className="mt-3">
-                <h2 className="text-base text-neutral-900 font-semibold">Arsenal</h2>
-                <span className="block mt-1 text-sm text-neutral-500 dark:text-neutral-400">25 Articles</span>
-               </div></a>
-              <a className="nc-CardCategory2 relative flex flex-col items-center justify-center text-center px-3 py-5 sm:p-6 bg-white dark:bg-neutral-900 rounded-3xl transition-colors " href="/archive/the-demo-archive-slug">
-               <div className="relative flex-shrink-0 w-20 h-20 rounded-full shadow-lg overflow-hidden z-0">
-                <img src="https://cdn.sanity.io/images/wusctyxy/production/224657f681cd46365c894aaaa130f68221dd10d8-720x1280.jpg" alt="categories" sizes="80px" className="object-cover  object-cover absolute inset-0 w-full h-full" />
-               </div>
-               <div className="mt-3">
-                <h2 className="text-base text-neutral-900 font-semibold">Man United</h2>
-                <span className="block mt-1 text-sm text-neutral-500 dark:text-neutral-400">14 Articles</span>
-               </div></a>
-              <a className="nc-CardCategory2 relative flex flex-col items-center justify-center text-center px-3 py-5 sm:p-6 bg-white dark:bg-neutral-900 rounded-3xl transition-colors " href="/archive/the-demo-archive-slug">
-               <div className="relative flex-shrink-0 w-20 h-20 rounded-full shadow-lg overflow-hidden z-0">
-                <img src="https://cdn.sanity.io/images/wusctyxy/production/76ec97f5f6104a9a972b340c1e0aed93e9704700-540x960.jpg" alt="categories" sizes="80px" className="object-cover  object-cover absolute inset-0 w-full h-full" />
-               </div>
-               <div className="mt-3">
-                <h2 className="text-base text-neutral-900 font-semibold">Real Madrid</h2>
-                <span className="block mt-1 text-sm text-neutral-500 dark:text-neutral-400">4 Articles</span>
-               </div></a>
-              <a className="nc-CardCategory2 relative flex flex-col items-center justify-center text-center px-3 py-5 sm:p-6 bg-white dark:bg-neutral-900 rounded-3xl transition-colors " href="/archive/the-demo-archive-slug">
-               <div className="relative flex-shrink-0 w-20 h-20 rounded-full shadow-lg overflow-hidden z-0">
-                <img src="https://cdn.sanity.io/images/wusctyxy/production/02eb21feb5a7284c6f9d8fe6d46c594eadb5f4f8-375x750.jpg" alt="categories" sizes="80px" className="object-cover  object-cover absolute inset-0 w-full h-full" />
-               </div>
-               <div className="mt-3">
-                <h2 className="text-base text-neutral-900 font-semibold">Man City</h2>
-                <span className="block mt-1 text-sm text-neutral-500 dark:text-neutral-400">7 Articles</span>
-               </div></a>
-              <a className="nc-CardCategory2 relative flex flex-col items-center justify-center text-center px-3 py-5 sm:p-6 bg-white dark:bg-neutral-900 rounded-3xl transition-colors " href="/archive/the-demo-archive-slug">
-               <div className="relative flex-shrink-0 w-20 h-20 rounded-full shadow-lg overflow-hidden z-0">
-                <img src="https://cdn.sanity.io/images/wusctyxy/production/c1bf3800552b2947d22ab83d952cc71f35fc112d-414x736.jpg" alt="categories" sizes="80px" className="object-cover  object-cover absolute inset-0 w-full h-full" />
-               </div>
-               <div className="mt-3">
-                <h2 className="text-base text-neutral-900 font-semibold">Chelsea</h2>
-                <span className="block mt-1 text-sm text-neutral-500 dark:text-neutral-400">26 Articles</span>
-               </div></a>
-             </div>
-            </div>
-            <div className="text-center mx-auto mt-10 md:mt-16">
-             <button className="nc-Button flex-shrink-0 relative h-auto inline-flex items-center justify-center rounded-full transition-colors border-transparent bg-secondary-500 hover:bg-secondary-6000 text-secondary-50 text-sm sm:text-base font-medium py-3 px-4 sm:py-3.5 sm:px-6  ">Show me more</button>
-            </div>
-           </div>
+      <article className="post-card">
+        <Link
+                  href={`/post/${pathPrefix ? `${pathPrefix}/` : ""}${
+                    post.slug.current
+                  }`}
+                >
+        <a className="post-img-wrap">
+               {imageProps ? (
+                     <img loading="lazy" sizes="(max-width:432px) 400px, (max-width:696px) 600px, (max-width:767px) 671px, 400px" src={imageProps.src} alt="s" />
+                    ) : (
+                      <span classNameName="absolute left-1/2 top-1/2 h-16 w-16 -translate-x-1/2 -translate-y-1/2 text-gray-200">
+                        <PhotoIcon />
+                      </span>
+                    )}
+                
+                                    </a>
+              </Link>
+                                    <div className="post-info-wrap">
+                                        <h2 className="h4 post-title">
+           <Link
+                  href={`/post/${pathPrefix ? `${pathPrefix}/` : ""}${
+                    post.slug.current
+                  }`}
+                >       
+      <a>{post.title}</a>
+      </Link>
+                                        </h2>
+                                        <div className="post-meta text-s flex align-center">
+                                            <div className="author-avatars flex">
+         <a href="/" className="author-image-wrap" title="Harini Banerjee">
+      { post?.author?.image && ( 
+       <img src = { AuthorimageProps.src } alt = { post?.author?.name } />
+     )
+   }
+                                                </a>
+                                            </div>
+                                            <div className="author-names">
+         <a href="/">{post?.author?.name}</a>
+                                            </div>
+                                            <span className="read-time">2 min read</span>
+                                            <span className="comment-count">
+                                                <a href="" className="flex align-center #i-comment"><svg>
+                                                        {/*<use xlink:href="#i-comment"></use>*/}
+                                                    </svg>
+                                                    
+                                                </a>
+                                            </span>
+                                        </div>
+                                    </div>
+                                </article>
+                              
    
     </>
   );
